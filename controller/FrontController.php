d72cbd70d7b8eeb148639be2e3842f13f4f4a9de
Bo <bophatin@gmail.com>
2019-02-12 15:01:46 +0100

    Ajout fichier controller page front

---
+++ controller/FrontController.php
@@ -0,0 +1,30 @@
+<?php  
+
+class FrontController {
+	
+  	public static function getListArticles() {
+			$listArt = new ArticleManager();
+			$articles = $listArt->getListArt();
+			require 'view/eauxView.php';
+		}
+		
+		public static function getListMenu() {
+			$test = new CategoryManager();
+			$ok = $test->getListCat();
+			require 'view/headerView.php';
+		}
+
+		public static function getArticle() {
+			if (isset($_GET['id']) AND !empty($_GET['id'])) {
+				$id = htmlspecialchars(($_GET['id']));
+				$article = new ArticleManager();
+				$post = $article->getArt($id);
+
+				if ($post == false) {
+					require 'view/404View.php';
+				} else {
+					require 'view/articleView.php';
+				}
+			}
+		}
+}
