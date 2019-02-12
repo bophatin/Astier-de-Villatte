3bf11a60d83a3a69ea7c32fd620e9227db372624
Bo <bophatin@gmail.com>
2019-02-12 15:16:53 +0100

    Ajout view footer

---
+++ view/footerView.php
@@ -0,0 +1,19 @@
+<footer id="wrapper-footer">
+	<div class="container-footer">
+		<div class="container-txt-footer">
+			<div class="left-container-footer">
+				<ul>
+					<li><a href="pro.php">Espace Pro</a></li>
+				</ul>
+				<p class="txt-footer">Astier de Villatte, 2019</p>
+			</div>
+			<div class="right-container-footer">
+				<ul class="rs-icons">
+					<li><i class="fab fa-facebook-f"></i></li>
+					<li><i class="fab fa-pinterest-p"></i></li>
+					<li><i class="fab fa-instagram"></i></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
