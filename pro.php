10b82fa790466f4c9ccd577d6a690d682bea1266
Bo <bophatin@gmail.com>
2019-02-12 15:01:05 +0100

    Ajout index page pro

---
+++ pro.php
@@ -0,0 +1,22 @@
+<?php
+require 'model/Autoloader.php';
+require 'controller/ProController.php';
+Autoloader::register();
+
+if(isset($_GET['page'])) {
+	$page = $_GET['page'];
+} else {
+	$page = 'proIndexView';
+}
+
+$page === isset($_GET['page']);
+
+switch($page) {
+	case 'proIndexView':
+	ProController::getUsers();
+	break;
+	
+    case 'log';
+	ProController::logPro();
+	break;
+}
