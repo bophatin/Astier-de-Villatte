a755225d7616ddb5178a097a702591c6aa091c13
Bo <bophatin@gmail.com>
2019-02-12 15:17:51 +0100

    Ajout view header

---
+++ view/headerView.php
@@ -0,0 +1,22 @@
+<header>
+	<div id="wrapper-header">
+		<div class="container-header">
+			<div class="logo"><a href="index.php"><img src="public/img/logo.png" alt="Astier de Villatte"></a></div>
+			<div class="container-nav">
+				<nav>
+					<ul class="menu-principal">
+						<li class="menu-collection"><a href="">Collection</a>
+							<ul class="submenu">
+								<li><a href="index.php?page=bougiesView">Bougies</a></li>
+								<li><a href="index.php?page=ceramicView">Ceramic</a></li>
+								<li><a href="index.php?page=eauxView">Eaux</a></li>
+							</ul>
+						</li>
+						<li class="menu-boutiques"><a href="">Boutiques</a></li>
+						<li class="menu-about"><a href="index.php?page=about">About</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
