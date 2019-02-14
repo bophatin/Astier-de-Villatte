3331a92ec70532319f9e03dfa9bddcd14c886ac3
Bo <bophatin@gmail.com>
2019-02-14 15:31:13 +0100

    Ajout modèle type page

---
+++ template/templatePage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="public/index.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Cormorant:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+	<?php require("view/headerView.php"); ?>
+    <?= $content; ?>
+	<?php include("view/footerView.php"); ?>
+
+	<script src="public/js/anime.min.js"></script>
+	<script src="public/js/animation.js"></script>
+</body>
+
+
+
+</html>
