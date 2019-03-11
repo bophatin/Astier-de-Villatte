e30218e60d6146c0d36b0b7157e98113da6cc677
Bo <bophatin@gmail.com>
2019-03-11 17:12:28 +0100

    Ajout vue toutes les images

---
+++ view/back/adminMediaView.php
@@ -0,0 +1,14 @@
+<?php $css='public/adminedit.css' ?>
+<?php $title='Back Office - Astier de Villatte' ?>
+
+
+<?php ob_start(); ?>
+    <h1>Media</h1>
+   
+
+
+
+   
+<?php $content = ob_get_clean(); ?>
+
+<?php require ('template/templateAdmin.php') ?>
