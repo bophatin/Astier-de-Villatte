cc3498f10a18c827ad990c842f621632aadd4d15
Bo <bophatin@gmail.com>
2019-03-26 18:24:09 +0100

    Ajout html page boutiques

--- view/boutiquesView.php
+++ view/boutiquesView.php
@@ -2,36 +2,34 @@
 <?php $title='Boutiques - Astier de Villatte'; ?>
 
 <?php ob_start(); ?>
-    <h1>Boutiques</h1>
 
-    <div id="form_contact">
-        <form action="index.php?page=action" id="contact" method="POST">
-            <p>
-                <label for="nom" class="nom">Nom</label>
-                <br /><input id="nom" name="nom" type="text">
-                <span id="msg_nom"></span>
-            </p>
-            <p>
-                <label for="sujet" class="sujet">Sujet</label>
-                <br /><input id="sujet" name="sujet" type="text">
-                <span id="msg_sujet"></span>
-            </p>
-            <p>
-                <label for="email">Email</label>
-                <br /><input id="email" name="email" type="email">
-                <span id="msg_email"></span>
-            </p>
-            <p>
-                <label for="message">Message</label>
-                <br /><textarea id="message" name="message" rows="10" cols="80"></textarea>
-            <span id="msg_message"></span>
-            </p>
-            <p>
-                <input type="submit" name ="submit" value="Envoyer" />
-            </p>
-        </form>
-        <span id="msg_all"></span>
+    <div id="wrapper_boutiques">
+        <div class="container_boutiques">
+            <div class="container_titres">
+                <p class="title-paris">A Paris,</p>
+                <h1>Boutiques</h1>
+            </div>
+            <img src="public/img/boutiques-astier.jpg" alt="Les boutiques Astier de Villatte"/>
+        </div>
     </div>
+
+    <div class="container_transition">
+        <p class="txt-transition">Maison parisienne depuis 1982, <br/>Astier de Villatte partage son histoire <br/>à travers ses 2 boutiques.</p>
+    </div>
+
+    <div id="wrapper-honore">
+        <div class="container-honore">
+            <div class="bloc-map"></div>
+            <div class="bloc-titres">
+                <div class="bloc-titre">
+                    <p class="rue">Rue</p>
+                    <p class="nom_shop">Saint <br/>Honore</p>
+                </div>
+            </div>
+            <div class="bloc-img"><img src="public/img/boutique-honore-astier.jpg" alt="Boutique Astier de Villatte rue Saint Honore Paris"></div>
+        </div>
+    </div>
+
 
 <?php $content= ob_get_clean(); ?>
 <?php require ('template/templatePage.php'); ?>
