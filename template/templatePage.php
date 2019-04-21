266520975e5d67a7b6c1ff29ee15a3f146579056
Bopha TIN <Bo@Bophas-iMac.local>
2019-04-21 14:40:13 +0200

    Ajout RS card + meta

--- template/templatePage.php
+++ template/templatePage.php
@@ -4,6 +4,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Découvrez les collections d'objets de décorations Astier de Villatte">
+	<meta property="og:title" content="Astier de Villatte">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://www.P5.bophatin.com/">
+	<meta property="og:image" content="public/img/logo.jpg">
+	<meta property="og:description" content="Découvrez les collections d'objets de décorations Astier de Villatte">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:site" content="@astierdevillatte">
+	<meta name="twitter:title" content="Astier de Villatte">
+	<meta name="twitter:description" content="Découvrez les collections d'objets de décorations Astier de Villatte">
+	<meta name="twitter:creator" content="@astierdevillatte">
+	<meta name="twitter:image" content="public/img/logo.jpg">
 	<link rel="stylesheet" type="text/css" href="<?= $css; ?>"/>
 	<link rel="stylesheet" type="text/css" href="public/template.css"/>
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
