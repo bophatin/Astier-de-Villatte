78924eb6e2c14a84de1bf694b0891b467aa2fb12
Bo <bophatin@gmail.com>
2019-04-21 11:50:36 +0200

    Recup variable menu

--- view/headerView.php
+++ view/headerView.php
@@ -1,27 +1,24 @@
-<!DOCTYPE html>
-<html lang="fr-FR">
+<?php $newListMenu = new CategoryManager(); $menu = $newListMenu->getListCat(); ?>
 
 <header>
-	<div id="wrapper-header">
-		<div class="container-header">
-			<div class="logo"><a href="index.php"><img src="public/img/logo.png" alt="Astier de Villatte"></a></div>
-			<div class="container-nav">	
-				<nav>
-					<ul class="menu-principal">
-						<li class="menu-collection">Collections
-							<ul class="submenu">
-								<?php foreach($menu as $menus): ?>
-								<li><a href="index.php?page=allArticles&id=<?= $menus->id(); ?>"><?= $menus->nameCat(); ?></a></li>
-								<?php endforeach ?>
-							</ul>
-						</li>
-						<li class="menu-boutiques"><a href="index.php?page=boutiques">Boutiques</a></li>
-						<li class="menu-about"><a href="index.php?page=about">Ivan & Benoit</a></li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	</div>
+     <div id="wrapper-header">
+        <div class="container-header">
+            <div class="logo"><a href="index.php"><img src="public/img/logo.png" alt="Astier de Villatte"></a></div>
+            <div class="container-nav">	
+                <nav>
+                    <ul class="menu-principal">
+                        <li class="menu-collection">Collections
+                            <ul class="submenu">
+                                <?php foreach($menu as $menus): ?>
+                                <li><a href="index.php?page=allArticles&id=<?= $menus->id(); ?>"><?= $menus->nameCat(); ?></a></li>
+                                <?php endforeach ?>
+                            </ul>
+                        </li>
+                        <li class="menu-boutiques"><a href="index.php?page=boutiques">Boutiques</a></li>
+                        <li class="menu-about"><a href="index.php?page=about">En savoir plus</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </header>
-
-</html>
